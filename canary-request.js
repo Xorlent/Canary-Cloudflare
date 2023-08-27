b6b1fa0f25fc3dc9be28b452fa21d87e487eb4db
Xorlent <94985297+Xorlent@users.noreply.github.com>
2023-08-26 20:46:17 -0700

    Add files via upload

---
+++ canary-request.js
@@ -0,0 +1,29 @@
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+  // check we have a POST request
+  if (request.method !== "GET") {    
+    return new Response(``, {status: 200}) 
+  }
+  try {
+    const values = await canaryblocks.list()
+    const keys = values.keys;
+
+    if (!keys) {
+      return new Response(``, {status: 200})
+      }
+    
+    const length = keys.length
+    var content = keys[0].name
+
+    for (let i = 1; i < length; i++) {
+      content = content + `\n` + keys[i].name
+    }
+    return new Response(content, {status: 200})
+  }
+  catch (e) {
+    return new Response(``, {status: 200})
+  }
+}
